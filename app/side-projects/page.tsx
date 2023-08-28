c5a5f54c6d245dfb57b469e5eadecf001638dbe9
Jeff Soriano <soriano.jeffm@gmail.com>
2023-08-28 13:45:38 -0700

    Fill out side projects section

--- app/side-projects/page.tsx
+++ app/side-projects/page.tsx
@@ -1,3 +1,17 @@
 export default function SideProjects() {
-  return <section className="p-24 text-center">side projects</section>
+  return (
+    <section>
+      <h1 className="text-3xl font-bold">Coming soon...</h1>
+      <p className="mt-2">
+        There's nothing yet here that accurately displays my current skills, but
+        in the past I've built a simple CRUD app to document your pets using
+        React and NodeJS, as well as a mini-game for an Among Us themed
+        halloween party using straight up Javascript and jQuery.
+      </p>
+      <p className="mt-2">
+        I'm currently teaching myself NextJS so my next projects will be based
+        around that. Check back here soon!
+      </p>
+    </section>
+  )
 }
