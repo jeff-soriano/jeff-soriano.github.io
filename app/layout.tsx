d6c4745c38aeefe6517dc4017c11cd6f53a7af01
Jeff Soriano <soriano.jeffm@gmail.com>
2023-09-05 16:17:22 -0700

    Change layout for other pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,25 +23,10 @@ export default function RootLayout({
           crossOrigin="anonymous"
         ></script>
       </head>
-      <body className={`${inter.className}`}>
-        <div className="flex flex-col items-center justify-center bg-gradient-to-r from-neutral-900 to-stone-950 text-zinc-100 h-screen w-screen">
-          <nav className="my-16">
-            <ul className="flex items-center justify-center gap-4 text-sm">
-              <li className="text-zinc-500 duration-500 hover:text-zinc-300">
-                <Link href="/about-me">About me</Link>
-              </li>
-              <li className="text-zinc-500 duration-500 hover:text-zinc-300">
-                <Link href="/experience">Experience</Link>
-              </li>
-              <li className="text-zinc-500 duration-500 hover:text-zinc-300">
-                <Link href="/projects">Projects</Link>
-              </li>
-            </ul>
-          </nav>
-          <main>
-            <div>{children}</div>
-          </main>
-        </div>
+      <body
+        className={`${inter.className} bg-gradient-to-r from-neutral-900 to-stone-950 text-zinc-100 `}
+      >
+        {children}
       </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,42 @@
+'use client'
+
 import { Nunito_Sans } from 'next/font/google'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
 const nunitoSans = Nunito_Sans({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center">
-      <h1
-        className={`${nunitoSans.className} font-bold text-4xl sm:text-6xl md:text-9xl transition-opacity duration-1000 opacity-100 animate-bounce`}
-      >
-        wassup
-      </h1>
-      <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-zinc-500">
-          Hi, my name is Jeff. I'm a fullstack developer looking to build stuff
-          that makes a positive impact
-        </h2>
-      </div>
-    </section>
+    <div className="flex flex-col items-center justify-center h-screen w-screen">
+      <nav className="my-16">
+        <ul className="flex items-center justify-center gap-4 text-sm">
+          <li className="text-zinc-500 duration-500 hover:text-zinc-300">
+            <Link href="/about-me">About me</Link>
+          </li>
+          <li className="text-zinc-500 duration-500 hover:text-zinc-300">
+            <Link href="/experience">Experience</Link>
+          </li>
+          <li className="text-zinc-500 duration-500 hover:text-zinc-300">
+            <Link href="/projects">Projects</Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <section className="flex flex-col items-center">
+          <h1
+            className={`${nunitoSans.className} font-bold text-4xl sm:text-6xl md:text-9xl transition-opacity duration-1000 opacity-100 animate-bounce`}
+          >
+            wassup
+          </h1>
+          <div className="my-16 text-center animate-fade-in">
+            <h2 className="text-zinc-500">
+              Hi, my name is Jeff. I'm a fullstack developer looking to build
+              stuff that makes a positive impact
+            </h2>
+          </div>
+        </section>
+      </main>
+    </div>
   )
 }
