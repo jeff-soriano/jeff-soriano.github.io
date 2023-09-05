4a3cb516696849ce5da787f62a96742ef974dc0e
Jeff Soriano <soriano.jeffm@gmail.com>
2023-09-05 16:03:20 -0700

    Implement new homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,57 +23,23 @@ export default function RootLayout({
           crossOrigin="anonymous"
         ></script>
       </head>
-      <body
-        className={`${inter.className} flex flex-col items-center content-center bg-gradient-to-r from-neutral-900 to-stone-950 text-zinc-100`}
-      >
-        <div className="total-width h-screen" style={{ maxWidth: '1024px' }}>
-          <main className="main-padding pt-14 h-full">
-            <section className="flex flex-col h-full">
-              <div className="flex pb-4 header">
-                <h1 className="text-center text-5xl font-bold main-section">
-                  <Link className="nav-link" href="/">
-                    JS
-                  </Link>
-                </h1>
-                <nav className="mobile-nav text-center">
-                  <Link href="/about-me" className="nav-link">
-                    About me
-                  </Link>
-                  <span className="mx-2">|</span>
-                  <Link href="/experience" className="nav-link">
-                    Experience
-                  </Link>
-                  <span className="mx-2">|</span>
-                  <Link href="/projects" className="nav-link">
-                    Projects
-                  </Link>
-                </nav>
-              </div>
-              <div className="flex justify-between content-height">
-                <section className="px-4 pt-4 border-l-8 border-t-8 col-span-2 border-cyan-700 rounded overflow-scroll main-section">
-                  {children}
-                </section>
-                <nav className="flex flex-col nav">
-                  <ul className="px-4 pb-4 border-r-4 border-b-4 border-cyan-700 rounded mt-auto ml-auto self-end">
-                    <li>
-                      <Link href="/about-me" className="nav-link">
-                        About me
-                      </Link>
-                    </li>
-                    <li>
-                      <Link href="/experience" className="nav-link">
-                        Experience
-                      </Link>
-                    </li>
-                    <li>
-                      <Link href="/projects" className="nav-link">
-                        Projects
-                      </Link>
-                    </li>
-                  </ul>
-                </nav>
-              </div>
-            </section>
+      <body className={`${inter.className}`}>
+        <div className="flex flex-col items-center justify-center bg-gradient-to-r from-neutral-900 to-stone-950 text-zinc-100 h-screen w-screen">
+          <nav className="my-16">
+            <ul className="flex items-center justify-center gap-4 text-sm">
+              <li className="text-zinc-500 duration-500 hover:text-zinc-300">
+                <Link href="/about-me">About me</Link>
+              </li>
+              <li className="text-zinc-500 duration-500 hover:text-zinc-300">
+                <Link href="/experience">Experience</Link>
+              </li>
+              <li className="text-zinc-500 duration-500 hover:text-zinc-300">
+                <Link href="/projects">Projects</Link>
+              </li>
+            </ul>
+          </nav>
+          <main>
+            <div>{children}</div>
           </main>
         </div>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,21 @@
-import Image from 'next/image'
+import { Nunito_Sans } from 'next/font/google'
+
+const nunitoSans = Nunito_Sans({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <section>
-      <div>
-        <h1 className="text-4xl font-bold text-white">
-          Wassup, my name is Jeff Soriano
-        </h1>
-        <p className="mt-2">
-          I'm a fullstack developer based out of Las Vegas, NV. I have over 7
-          years of experience working across different tech stacks, but my core
-          strengths revolve around <strong>React</strong> and{' '}
-          <strong>Ruby on Rails</strong>.
-        </p>
-        <p className="mt-2">
-          If you want to build something together, hit me up! ✌️
-        </p>
+    <section className="flex flex-col items-center">
+      <h1
+        className={`${nunitoSans.className} font-bold text-4xl sm:text-6xl md:text-9xl transition-opacity duration-1000 opacity-100 animate-bounce`}
+      >
+        wassup
+      </h1>
+      <div className="my-16 text-center animate-fade-in">
+        <h2 className="text-zinc-500">
+          Hi, my name is Jeff. I'm a fullstack developer looking to build stuff
+          that makes a positive impact
+        </h2>
       </div>
-      <div className="text-5xl my-4">
-        <a href="mailto:soriano.jeffm@gmail.com" rel="noreferrer">
-          <i className="fas fa-envelope contact-icon"></i>
-        </a>
-        <a
-          href="https://github.com/jeff-soriano"
-          target="_blank"
-          rel="noreferrer"
-          className="hover:underline"
-        >
-          <i className="fab fa-github ml-8 contact-icon"></i>
-        </a>
-        <a
-          href="https://www.linkedin.com/in/jeff-soriano/"
-          target="_blank"
-          rel="noreferrer"
-          className="hover:underline"
-        >
-          <i className="fab fa-linkedin ml-8 contact-icon"></i>
-        </a>
-      </div>
-      <p className="mt-2">-JS</p>
     </section>
   )
 }
