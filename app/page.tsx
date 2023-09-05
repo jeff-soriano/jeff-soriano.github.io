01aa6dc6630c5189d844c14159575a47ec9dfd55
Jeff Soriano <soriano.jeffm@gmail.com>
2023-09-05 16:35:16 -0700

    Add fade in animation

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 
 import { Nunito_Sans } from 'next/font/google'
 import Link from 'next/link'
-import { useEffect, useState } from 'react'
 
 const nunitoSans = Nunito_Sans({ subsets: ['latin'] })
 
@@ -25,11 +24,11 @@ export default function Home() {
       <main>
         <section className="flex flex-col items-center">
           <h1
-            className={`${nunitoSans.className} font-bold text-4xl sm:text-6xl md:text-9xl transition-opacity duration-1000 opacity-100 animate-bounce`}
+            className={`${nunitoSans.className} text-white font-bold text-4xl sm:text-6xl md:text-9xl typewriter animate-fade-in`}
           >
             wassup
           </h1>
-          <div className="my-16 text-center animate-fade-in">
+          <div className="my-16 text-center">
             <h2 className="text-zinc-500">
               Hi, my name is Jeff. I'm a fullstack developer looking to build
               stuff that makes a positive impact
